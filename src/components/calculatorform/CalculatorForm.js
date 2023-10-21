89d7c0b2b3148276853d09b22152f3dbd01e3c90
shiva508 <dasarishiva1@gmail.com>
2023-10-21 16:18:17 +0530

    practice done by me

--- src/components/calculatorform/CalculatorForm.js
+++ src/components/calculatorform/CalculatorForm.js
@@ -1,44 +1,56 @@
 import { useState } from "react";
 import styles from "./CalculatorForm.module.css";
+const INITIAL = {
+  currentSavings: 1200,
+  yearlyContribution: 1200,
+  expectedReturn: 230,
+  duration: 12,
+};
 const CalculatorForm = (props) => {
   const [currentSavings, setCurrentSavings] = useState(0);
   const [yearlyContribution, setYearlyContribution] = useState(0);
   const [expectedReturn, setExpectedReturn] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [calcution, setCalcution] = useState(INITIAL);
 
   const yearCalcutionTrigger = (event) => {
     event.preventDefault();
-    const calcution = {
-      currentSavings: parseInt(currentSavings),
-      yearlyContribution: parseInt(yearlyContribution),
-      expectedReturn: parseInt(expectedReturn),
-      duration: parseInt(duration),
-    };
+    //   const calcution = {
+    //     currentSavings: parseInt(currentSavings),
+    //     yearlyContribution: parseInt(yearlyContribution),
+    //     expectedReturn: parseInt(expectedReturn),
+    //     duration: parseInt(duration),
+    //   };
     props.newYearCalculation(calcution);
   };
   const commonDataChangeHandler = (attribute, value) => {
-    if (attribute === "current-savings") {
-      setCurrentSavings((prevValue) => {
-        return +value;
-      });
-    }
-    if (attribute === "yearly-contribution") {
-      setYearlyContribution((prevValue) => {
-        return +value;
-      });
-    }
-    if (attribute === "expected-return") {
-      setExpectedReturn((prevValue) => {
-        return +value;
-      });
-    }
-    if (attribute === "duration") {
-      setDuration((prevValue) => {
-        return +value;
-      });
-    }
+    setCalcution((prevData) => {
+      return { ...prevData, [attribute]: value };
+    });
+
+    //   if (attribute === "current-savings") {
+    //     setCurrentSavings((prevValue) => {
+    //       return +value;
+    //     });
+    //   }
+    //   if (attribute === "yearly-contribution") {
+    //     setYearlyContribution((prevValue) => {
+    //       return +value;
+    //     });
+    //   }
+    //   if (attribute === "expected-return") {
+    //     setExpectedReturn((prevValue) => {
+    //       return +value;
+    //     });
+    //   }
+    //   if (attribute === "duration") {
+    //     setDuration((prevValue) => {
+    //       return +value;
+    //     });
+    //   }
   };
   const resetHandler = () => {
+    setCalcution(INITIAL);
     props.resetYearCalculation();
   };
   return (
@@ -50,8 +62,9 @@ const CalculatorForm = (props) => {
             <input
               type="number"
               id="current-savings"
+              value={calcution.currentSavings}
               onChange={(event) =>
-                commonDataChangeHandler("current-savings", event.target.value)
+                commonDataChangeHandler("currentSavings", event.target.value)
               }
             ></input>
           </p>
@@ -60,9 +73,10 @@ const CalculatorForm = (props) => {
             <input
               type="number"
               id="yearly-contribution"
+              value={calcution.yearlyContribution}
               onChange={(event) =>
                 commonDataChangeHandler(
-                  "yearly-contribution",
+                  "yearlyContribution",
                   event.target.value
                 )
               }
@@ -77,8 +91,9 @@ const CalculatorForm = (props) => {
             <input
               type="number"
               id="expected-return"
+              value={calcution.expectedReturn}
               onChange={(event) =>
-                commonDataChangeHandler("expected-return", event.target.value)
+                commonDataChangeHandler("expectedReturn", event.target.value)
               }
             />
           </p>
@@ -87,6 +102,7 @@ const CalculatorForm = (props) => {
             <input
               type="number"
               id="duration"
+              value={calcution.duration}
               onChange={(event) =>
                 commonDataChangeHandler("duration", event.target.value)
               }
