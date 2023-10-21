3946f7b1db0d25409a073b428f00a56e1ee70253
shiva508 <dasarishiva1@gmail.com>
2023-10-21 22:24:35 +0530

    second pactice

---
+++ src/components/userslist/UserList.js
@@ -0,0 +1,21 @@
+import Card from "../../ui/card/Card";
+import styles from "./UserList.module.css";
+const UserList = (props) => {
+  const content = <p className={styles.emptyContent}>Please add some users</p>;
+  if (props?.users?.length > 0) {
+    content = (
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li key={user.age + user.name}>
+              {" "}
+              {user.name} ({user.age}){" "}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+  return <Card className={styles.users}>{content}</Card>;
+};
+export default UserList;
