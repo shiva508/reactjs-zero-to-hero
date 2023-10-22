70f533b65bbeb3cdbaff74fff476d304b4174a3e
shiva508 <dasarishiva1@gmail.com>
2023-10-22 06:44:54 +0530

    code clenup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import AddUser from "./components/adduser/AddUser";
-import UserList from "./components/userslist/UserList";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const addNewUserHandler = (user) => {
-    setUsers((prevUser) => {
-      return [...prevUser, user];
-    });
-  };
   return (
-    <div className="user-profile">
-      <AddUser addNewUser={addNewUserHandler}></AddUser>
-      <UserList users={users}></UserList>
+    <div>
+      <h1>React Js Practice</h1>
     </div>
   );
 }
--- src/ui/button/Button.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Button.module.css";
-const Button = (props) => {
-  return (
-    <button className={styles.button} type={props.type} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
-export default Button;
